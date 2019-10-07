96393e027aec22b4904891cd8f96572398216005
daniel.lulka <daniel.lulka@bbva.com>
2019-10-07 17:03:26 -0500

    Second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,14 @@ public class Main {
     }
     public static String WhoIsNext(String[] names, int n) {
         String name = null;
-        String[] answer = {};
-        names[(names.length)+1] = names[0];
+        int i;
+        String answer[] = new String[names.length + 1];
+        while(i < names.length)
+        System.out.println("Answers: " + answer.length);
+        System.out.println("Names: " + names.length);
+        names = new String[]{};
+        answer[(answer.length)+1] = names[0];
+        System.out.println(answer.length);
      return name;
     }
 }

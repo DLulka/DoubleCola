2cc83a50f13ea20d47e260ac2f8e25f7925a37cb
daniel.lulka <daniel.lulka@bbva.com>
2019-10-07 16:54:05 -0500

    First

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Main.WhoIsNext(new String[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" }, 10));
+    }
+    public static String WhoIsNext(String[] names, int n) {
+        String name = null;
+        String[] answer = {};
+        names[(names.length)+1] = names[0];
+     return name;
+    }
+}
